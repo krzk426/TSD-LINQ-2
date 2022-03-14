6015bc9b7acf2bce146b3fa7e63e319a381478f6
krzk426 <krzk426@gmail.com>
2022-03-14 19:05:26 +0100

    develop task_2_4

--- generic/generic/Program.cs
+++ generic/generic/Program.cs
@@ -15,37 +15,4 @@
             Console.WriteLine(ifLeap(new DateTime(1600, 1, 1)));
         }
     }
-
-    class GenericCollection <T>
-    {
-        private List<T> RandomList;
-        private Random value;
-
-        public GenericCollection()
-        {
-            RandomList = new List<T>();
-            value = new Random();
-        }
-        public bool ifEmpty()
-        {
-            return RandomList.Any();
-        }
-        public T Get(int i)
-        {
-            return RandomList[value.Next(1, 100)];
-        }
-        public void Add(T el)
-        {
-            if(value.Next(1,100) > 25)
-            {
-                RandomList.Insert(0, el);
-            }
-            else
-            {
-                RandomList.Add(el);
-            }
-        }
-    }
-
-
 }
